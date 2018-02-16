817317353f096a4e32952c2126faff2c625d0dc6
Anthony Schneider <AnthonySchneider@Anthonys-MacBook-Pro-2.local>
2018-02-16 13:00:18 -0500

    update test_minmax.py to check for exceptions

--- test_minmax.py
+++ test_minmax.py
@@ -1,8 +1,24 @@
 def test_minmax():
-    from sum_num import find_extremes
-    FindExtremesOutput1 = find_extremes([0, -1.5, -2, -7, -53])
-    FindExtremesOutput2 = find_extremes([0, 3.2, 2, 10, 6])
-    FindExtremesOutput3 = find_extremes([2, -3, 54, 6, 10])
-    assert FindExtremesOutput1 == (-53, 0)
-    assert FindExtremesOutput2 == (0, 10)
-    assert FindExtremesOutput3 == (-3, 54)
+    from minmax import findextremes
+    from minmax import contains_imaginary
+    from minmax import check_list
+    from minmax import check_input
+    import pytest
+    findextremes_Output1 = findextremes([0, -1.5, -2, -7, -53])
+    findextremes_Output2 = findextremes([0, 3.2, 2, 10, 6])
+    findextremes_Output3 = findextremes([2, -3, 54, 6, 10])
+    assert findextremes_Output1 == [-53.0, 0.0]
+    assert findextremes_Output2 == [0, 10]
+    assert findextremes_Output3 == [-3.0, 54.0]
+
+    def test_imaginary_input():
+        with pytest.raises(ValueError):
+            findextremes([3+2j, 4])
+
+    def test_empty_input():
+        with pytest.raises(TypeError):
+            findextremes([])
+
+    def test_non_list_in():
+        with pytest.raises(TypeError):
+            findextremes('Hello World')
